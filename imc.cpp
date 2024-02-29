acdcb7a1235c840120531bf7cb95de37db777d8f
dyva101 <B51115@fgv.edu.br>
2024-02-29 20:20:38 -0300

    finalizado

--- imc.cpp
+++ imc.cpp
@@ -15,5 +15,25 @@ int main() {
   // Cálculo do IMC
   imc = peso / (altura * altura);
 
+    // Determinação da classificação
+  if (imc < 17) {
+    classificacao = "Muito Abaixo do Peso (que nem eu)";
+  } else if (imc < 18.5) {
+    classificacao = "Abaixo do peso";
+  } else if (imc < 25) {
+    classificacao = "Peso normal";
+  } else if (imc < 30) {
+    classificacao = "Acima do peso";
+  } else if (imc < 35) {
+    classificacao = "Obesidade";
+  } else if (imc < 40) {
+    classificacao = "Obesidade Severa";
+  } else {
+    classificacao = "Obesidade Mórbida";
+  }
+
+  // Exibição do IMC e classificação
+  cout << "Seu IMC é " << fixed << imc << " e você está " << classificacao << "." << endl;
+
   return 0;
 }
